fe242445a8fce4504b54b073893cdf031f7f56b9
Rydham Dangayach <25bcm054@nirmauni.ac.in>
2026-06-14 23:09:58 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const rateLimit = require('express-rate-limit')
 
 const app = express()
 const PORT = process.env.PORT || 3000
+app.set('trust proxy',1)
 
 // ── Security Middleware ──────────────────────
 app.use(helmet())
